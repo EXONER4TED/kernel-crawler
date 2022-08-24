2855bf644eaca9cbc902fafd551dd86f0946baab
Logan Bond <lbond@secureworks.com>
2022-08-24 17:51:37 -0500

    print something

--- kernel_crawler/redhat.py
+++ kernel_crawler/redhat.py
@@ -15,6 +15,7 @@ class RedhatContainer(repo.ContainerDistro):
 
         cmd = ['repoquery', '--show-duplicates', 'kernel-devel']
         process = subprocess.run(cmd, stdout=PIPE, stderr=PIPE)
+        print('banana')
         print(process)
 
         cmd_out = process.stdout.decode('utf-8')
